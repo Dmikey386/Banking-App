6868108d7efe6cdbc64a1fc3e6717bbd42fbbf48
Michael Dodd <mpdodd2@gmail.com>
2025-03-25 18:12:38 -0400

    Implemented getObjectMap to return Map of object attribute, Implemented getUser with getObjectMap

--- Bank/core/Main.java
+++ Bank/core/Main.java
@@ -6,8 +6,7 @@ import Bank.user.User;
 public class Main {
     public static void main(String[] args) throws IOException {
         Bank bank = new Bank();
-
-        User user = bank.userLogger.getObject("USER30405439476727808");
+        User user = bank.userLogger.getUser("USER30405439476727808");
         System.out.println(user.getUserID());
 
     }
