e22659fe98eafa4084ec935627074fc1df53803a
Michael Dodd <mpdodd2@gmail.com>
2025-03-25 16:18:07 -0400

    Created JsonLogger Class, and implemented write function to edit Storage json files

--- Bank/JsonStorage/JsonLogger.java
+++ Bank/JsonStorage/JsonLogger.java
@@ -1,4 +1,18 @@
 package Bank.JsonStorage;
 
-public class JsonLogger {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
+
+public class JsonLogger<T> {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+
+    public void jsonifyLogger(T logger) throws IOException {
+       mapper.writerWithDefaultPrettyPrinter().writeValue(new File("Storage/users.json"), logger);
+    }
+
+    public static void main(String[] args) throws IOException {
+    }
 }
