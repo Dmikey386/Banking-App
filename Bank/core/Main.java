d4eb957b55481c6e37746f3b41ca9f329f9effb1
Michael Dodd <mpdodd2@gmail.com>
2025-03-26 14:07:14 -0400

    Tests I did bc i was proud

--- Bank/core/Main.java
+++ Bank/core/Main.java
@@ -7,9 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Bank bank = new Bank();
 
-        // transfer
-        String[] accounts = {"BA30708702289854464","BA307087022342344464"};
-        bank.createTransaction("USER30708484903272448",accounts,5000,2);
+
 
     }
 }
