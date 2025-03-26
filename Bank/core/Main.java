87589eaf540fc2f1caa9a89edef471929e2312aa
Michael Dodd <mpdodd2@gmail.com>
2025-03-26 13:54:58 -0400

    Implemented wiretransfer to update logs correctly

--- Bank/core/Main.java
+++ Bank/core/Main.java
@@ -6,8 +6,10 @@ import Bank.user.User;
 public class Main {
     public static void main(String[] args) throws IOException {
         Bank bank = new Bank();
-        String[] accounts = {"BA30708702122082304"};
-        bank.createTransaction("USER30708484752277504",accounts,5000,1);
+
+        // transfer
+        String[] accounts = {"BA30708702122082304","BA30708702289854464"};
+        bank.createTransaction("USER30708484752277504",accounts,50000,2);
 
     }
 }
