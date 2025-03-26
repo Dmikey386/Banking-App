93122ec459ae54b763ea566387efaae9b1f9c19c
Michael Dodd <dodd.m@northeatern.edu>
2025-03-26 13:50:12 -0400

    Implemented withdraw to update loggs appropriately

--- Bank/transactions/Withdraw.java
+++ Bank/transactions/Withdraw.java
@@ -1,10 +1,19 @@
 package Bank.transactions;
 
+import Bank.account.BankAccount;
 import Bank.core.Bank;
+import java.io.IOException;
 
-//class Withdraw extends Transaction{
-//    // constructor
-//    public Withdraw(Bank bank, String[] accountIDs, double amount){
-//        super(bank, accountIDs, amount);
-//    }
-//}
+class Withdraw extends Transaction{
+    // constructor
+    public Withdraw(Bank bank, String[] accountIDs, double amount) throws IOException {
+        super(bank, accountIDs, amount);
+    }
+
+    @Override
+    public void process() throws IOException {
+        BankAccount[] accounts = getAccounts(accountIDs);
+        accounts[0].setBalance(accounts[0].getBalance() - amount);
+        updateLogs(accounts);
+    }
+}
