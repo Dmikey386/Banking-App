1543894a17dd248d4e581698896a9f157aa287cb
Michael Dodd <mpdodd2@gmail.com>
2025-03-28 14:51:17 -0400

    WIP: edits to AutoTransactionVerifier before merge

--- Bank/transactions/transactionProcessing/automatic/AutoTransactionVerifier.java
+++ Bank/transactions/transactionProcessing/automatic/AutoTransactionVerifier.java
@@ -1,11 +1,21 @@
 package Bank.transactions.transactionProcessing.automatic;
 
 import Bank.account.AccountLogger;
+import Bank.transactions.transactionProcessing.single.TransactionRequest;
+import Bank.transactions.transactionProcessing.single.TransactionVerifierException;
+import Bank.user.User;
 import Bank.user.UserLogger;
 
+import java.io.IOException;
+
 public class AutoTransactionVerifier{
-    private UserLogger userLogger = UserLogger.getInstance();
-    private AccountLogger accountLogger = AccountLogger.getInstance();
+    private AutoTransactionRequest transactionRequest;
+    private String userID;
+    private String[] bankAccountIDs;
+    private int transactionType;
+    private double amount;
+    private AccountLogger accuontLog = AccountLogger.getInstance();
+    private UserLogger userLog = UserLogger.getInstance();
 
     public AutoTransactionRequest verifyTransaction(AutoTransactionRequest autoTransactionRequest){
         try {
@@ -17,6 +27,8 @@ public class AutoTransactionVerifier{
         return autoTransactionRequest;
     }
 }
+
+
 
 class AutoTransactionException extends Exception{
     public AutoTransactionException(String message){
